62a8e7c5855c73c9372e7743cf3b4db18c20b5a8
Minh Tam <tamnm1996@gmail.com>
2022-09-05 11:10:12 +0700

    first commit

---
+++ CDMG-Project/CDMG-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CDMG-Project
+//
+//  Created by Nguyen Minh Tam on 05/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
